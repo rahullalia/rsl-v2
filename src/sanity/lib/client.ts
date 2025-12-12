6a379a5d618bb5cf6e22536b602d391d1df87fb6
rahullalia <rahul.lalia23@gmail.com>
2025-12-11 17:20:03 -0800

    Fix Vercel build: Hardcode public Sanity ID

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+export const projectId = '36wenybq'
+export const dataset = 'production'
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
 export const client = createClient({
